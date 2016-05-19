600a111cb311def2af42f5818cb26765cd34da76
Daniel Podrabinek <danyagrohman@gmail.com>
2016-05-19 14:30:04 +0300

    added socket.io js lib

--- Plugin.php
+++ Plugin.php
@@ -68,7 +68,7 @@ class Plugin extends PluginBase
     {
         $widget->addCss('https://cdn.jsdelivr.net/jquery.gritter/1.7.4/css/jquery.gritter.css');
         $widget->addJs('https://cdn.jsdelivr.net/jquery.gritter/1.7.4/js/jquery.gritter.min.js');
-        $widget->addJs('https://cdn.socket.io/socket.io-1.3.7.js');
+        $widget->addJs('/plugins/grohman/tattler/js/socket.io-1.3.7.min.js');
         $widget->addJs('/plugins/grohman/tattler/js/tattler.js',
             [ 'id' => 'tattlerJs', 'data-debug' => env('APP_DEBUG'), 'data-rooms' => json_encode($rooms) ]);
         $widget->addJs('/plugins/grohman/tattler/js/crud_handlers.js');
